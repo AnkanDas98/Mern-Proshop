95aafc0d80cd75a25c74ace3bcd64234d496c4db
Ankan Das <ankan.das96@gmail.com>
2022-11-21 15:11:59 +0600

    fix responsive design

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api";
+// const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://mernelectronics.herokuapp.com/api";
 
 // const Token = localStorage.getItem("userInfo")
 //   ? JSON.parse(localStorage.getItem("userInfo")).token
